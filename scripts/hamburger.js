bbe60380e9909316b46a776a21f1c147d137cc33
Tim Grootendorst <tim@compete.nl>
2025-01-20 22:58:48 +0100

    updated header

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,32 +1,30 @@
-const options = {
-  root: document.querySelector(".section-projects"),
-  rootMargin: "0px",
-  threshold: 1.0,
-};
+const mobileMenuElement = document.querySelectorAll(".mobile-line");
+const headerElement = document.querySelector(".sticky-nav");
+const projectElement = document.querySelector(".section-homepage");
 
-const changeColorMenu = function (entries, observer) {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (observer.IntersectionObserver) {
-      console.log("now");
-    }
-    if (entry.isIntersecting) {
-      console.log("this works 2");
-      // Change color or perform any action
-      entry.target.style.stroke = "white";
-    } else {
-      entry.target.style.stroke = "black";
-    }
-  });
-};
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting)
+        mobileMenuElement.forEach((element) => {
+          element.classList.remove("light");
+          element.classList.add("dark");
+          headerElement.classList.remove("sticky");
+        });
+      else {
+        mobileMenuElement.forEach((element) => {
+          element.classList.remove("dark");
+          element.classList.add("light");
+          headerElement.classList.add("sticky");
+        });
+      }
+    });
+  },
+  {
+    root: null,
+    rootMargin: "-10px",
+    threshold: 0,
+  }
+);
 
-const target = document.querySelectorAll(".mobile-line");
-
-const observer = new IntersectionObserver(changeColorMenu, options);
-
-target.forEach((element) => {
-  observer.observe(element);
-  console.log("this works");
-});
-
-console.log("this works");
+observer.observe(projectElement);
